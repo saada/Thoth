1869a5d0e136ce1dfaa0369d1143033e0d5e279f
Mahmoud Saada <mahmoudsaada@gmail.com>
2013-02-18 02:21:05 -0700

    remove chat.js

--- views/chat.js
+++
@@ -1,9 +0,0 @@
-var socket = io.connect('http://localhost');
-socket.on('all_rooms', function (data) {
-	console.log(data);
-	// socket.emit('my other event', { my: 'data' });
-});
-
-$(function(){
-	
-});

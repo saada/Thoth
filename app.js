7601fb18fd11e7b978a95fdb79a60d9f55feebe7
Mahmoud Saada <mahmoudsaada@gmail.com>
2013-03-01 03:50:55 -0700

    Improve express code

--- app.js
+++ app.js
@@ -9,23 +9,36 @@ var HTTPSERVER_PORT = 8000;
 var WEBRTC_PORT = 4000;
 var WEBSOCKET_PORT = 80;
 
-// add the express framework
+/**
+ * Module dependencies.
+ */
 var express = require('express'),
     app  = express(),
   server = require('http').createServer(app),
+    path = require('path'),
       // io = require('socket.io').listen(server),
   webRTC = require('webrtc.io'),
   routes = require('./routes');
 
 // Config
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieSession({cookie:{httpOnly: false}})); //allows client-side JS to access the session id
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
-app.use(express.bodyParser());
-app.use(express.static(__dirname + '/public'));
-app.use(express.cookieParser('aoeiwhvoiwehfaowe'));
-app.use(express.cookieSession({cookie:{httpOnly: false}})); //allows client-side JS to access the session id
-app.use(app.router);
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
 app.get('/', routes.index);
 app.get('/chat', checkAuth, routes.chat);

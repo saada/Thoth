56207490f040a2fd2c25c34eb7776e9f5161120e
Mahmoud Saada <mahmoudsaada@gmail.com>
2013-03-18 03:12:07 -0700

    Cookie session secured again

--- webserver.js
+++ webserver.js
@@ -19,7 +19,7 @@ app.configure(function(){
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser('your secret here'));
-	app.use(express.cookieSession({cookie:{httpOnly: false}})); //allows client-side JS to access the session id
+	app.use(express.cookieSession());
 	app.use(express.session());
 	app.use(app.router);
 	app.use(express.static(path.join(__dirname, 'public')));
